8410a40e0fd4bc99b21c3e9c71cc623332873184
Joni Moas <setpsistariasbaniou@gmail.com>
2025-02-26 22:23:44 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+small script that watches when a buffalo nas is available, mounts, updates the share and shuts off the nas afterwards.
+it also activates telnet for the duration of the backup for monitoring purposes.
+can be used as a cronjob or a systemd service with the included file.
+just replace <BUFFALOPASS> <BUFFALOSHARE> <LOCALSHARE> <ACPPATH> <BUFFALOIP> <SOURCE> <DEST>.
+requires acp commander to perform the telnet and shutdown functions.
+i may make a more cohesive configurable version if i am not bored :)
---
+++ backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+while ! mount -t cifs -o username=administrator,password=<BUFFALOPASS>,vers=2.0  <BUFFALOSHARE> <LOCALSHARE>;
+  do echo "mount not available"
+  umount -l <LOCALSHARE>
+  sleep 5
+done
+echo "mounted"
+sleep 60
+java -jar <ACPPATH>/acp_commander.jar -t <BUFFALOIP> -pw <BUFFALOPASS> -o
+mkdir -p <DEST>
+ulimit -m 1048576 && ionice -c2 -n7 rsync -a --partial --info=NAME --human-readable --timeout=600 --recursive <SOURCE> <DEST>
+umount <LOCALSHARE>
+echo "unmounted"
+sleep 60
+java -jar <ACPPATH>/acp_commander.jar -t <BUFFALOIP> -pw <BUFFALOPASS> -c shutdown
+sleep 60
